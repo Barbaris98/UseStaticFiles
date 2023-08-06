dda99bda775bb72fe92aba8b22d40604d4e69e0d
Dmitry Grigorev <d-d12@mail.ru>
2023-08-06 19:16:20 +0400

    тоже самое,но с методом UseFileServer()

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using System;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
+//app.UseDefaultFiles();  // поддержка страниц html по умолчанию
+app.UseFileServer(); //тоже самое
+
 app.UseStaticFiles(); // доб поддержку статических файлов
+//доб middleware с функционалом работы стат файлов
 
 app.Run(async (context) => await context.Response.WriteAsync("Hello World!"));
 
